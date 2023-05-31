b0902f1cf3135a89604200590c79d65eb4ff16ae
avinash84319 <avichinnu22@gmail.com>
2023-05-31 19:36:17 +0530

    added vision mission aim page

--- src/screens/Home2.js
+++ src/screens/Home2.js
@@ -12,11 +12,37 @@ function Home2(){
     });
 
     return <div className="home">
-        <video  className="home-video" src="/club-comp.mp4" autoPlay loop muted />
-        <div className="home-content">
-            <h1 className='home-content-h1'>STELLAR JC</h1>
-            <p className="home-content-p">A place where you can learn, grow and explore</p>
-            {/* <button className="home-content-btn">EXPLORE</button> */}
+        <div className="home-main">
+            <div className="home-main-aim">
+                <h1 className="home-main-aim-h1">AIM</h1>
+                <p className="home-main-aim-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                id est laborum.</p>
+           </div>
+           <div className="home-main-aim">
+                <h1 className="home-main-aim-h1">MISSION</h1>
+                <p className="home-main-aim-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                id est laborum.</p>
+           </div>
+           <div className="home-main-aim">
+                <h1 className="home-main-aim-h1">VISION</h1>
+                <p className="home-main-aim-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+                id est laborum.</p>
+           </div>
+           <h1 className="home-main-h1">MISSION</h1>
+           <h1 className="home-main-h1">AIM</h1>
+           <h1 className="home-main-h1">AIM</h1>
             <nav className="home-content-nav">
                 <Link to="/"><span className="home-content-nav-span">.</span></Link>
                 <Link to="/h2"><span className="home-content-nav-spana">.</span></Link>
