729958f3fc066acae2c749e51ceb2cc9c040fb76
avinash84319 <avichinnu22@gmail.com>
2023-05-31 18:42:21 +0530

    added the scroll functions and pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
+import Home2 from './screens/Home2';
+import Home3 from './screens/Home3';
 import Cosmology from './screens/Cosmology';
 import Rocketry from './screens/Rocketry';
 import Tech from './screens/Tech';
@@ -16,6 +18,8 @@ function App() {
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/h2" element={<Home2 />} />
+          <Route path="/h3" element={<Home3 />} />
           <Route path="/Cos" element={<Cosmology />} />
           <Route path="/roc" element={<Rocketry />} />
           <Route path="/spt" element={<Tech />} />
--- src/screens/Home3.js
+++ src/screens/Home3.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import '../css/Home.css';
 import { Link } from 'react-router-dom';
-function Home() {
+function Home3(){
 
     window.addEventListener('scroll', function (e) {
         console.log(this.window.scrollY);
-        if (this.window.scrollY >80) {
-            console.log('scrolling down');
-            window.location.href = '/h2';
-        }
     });
 
+
     return <div className="home">
-        <video className="home-video" src="/club-comp.mp4" autoPlay loop muted />
+        <video  className="home-video" src="/club-comp.mp4" autoPlay loop muted />
         <div className="home-content">
             <h1 className='home-content-h1'>STELLAR JC</h1>
             <p className="home-content-p">A place where you can learn, grow and explore</p>
             {/* <button className="home-content-btn">EXPLORE</button> */}
             <nav className="home-content-nav">
-                <Link to="/"><span className="home-content-nav-spana">.</span></Link>
+                <Link to="/"><span className="home-content-nav-span">.</span></Link>
                 <Link to="/h2"><span className="home-content-nav-span">.</span></Link>
-                <Link to="/h3"><span className="home-content-nav-span">.</span></Link>
+                <Link to="/h3"><span className="home-content-nav-spana">.</span></Link>
             </nav>
         </div>
         {/* <div className="home-b1"> */}
@@ -28,4 +25,4 @@ function Home() {
     </div>
 }
 
-export default Home;
+export default Home3;
