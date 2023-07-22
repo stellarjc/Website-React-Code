2c1c9a53b315aaf04039cb760e3d742f819cfeeb
avinash84319 <avichinnu22@gmail.com>
2023-07-22 09:33:08 +0530

    cosmology -done

--- src/screens/Cosmology.js
+++ src/screens/Cosmology.js
@@ -13,24 +13,24 @@ function Cosmology() {
     </div>
      <div className='cos-content'>
             <h1 className='cos-content-main-h1'>What do we do?</h1>
-            <p className='cos-content-main-p'>We focus on the various <span className='gradient-underline-pink'>astronomical and meteorological observations</span>, sky studies, operations of telescopes and special purpose sensors and cameras, composition of planets, motion of celestial bodies, eclipses, <span className="gradient-underline-blue">meteor showers, planetary alignments</span>, origin and evolution of the universe, astrobiology, <br /> Geeky astrophysical phenomena like <span className='gradient-underline-light-green'>black holes, neutron stars, nebulae, galaxy motions</span>, birth of starts and other concepts like gravity,space-time, twin paradox etc. To find the fundamental nature of the universe and our place within it</p>
+            <p className='cos-content-main-p'>We focus on the various <span className='gradient-underline-pinkk'>astronomical and meteorological observations</span>, sky studies, operations of telescopes and special purpose sensors and cameras, composition of planets, motion of celestial bodies, eclipses, <span className="gradient-underline-bluee">meteor showers, planetary alignments</span>, origin and evolution of the universe, astrobiology, <br /> Geeky astrophysical phenomena like <span className='gradient-underline-light-greenn'>black holes, neutron stars, nebulae, galaxy motions</span>, birth of starts and other concepts like gravity,space-time, twin paradox etc. To find the fundamental nature of the universe and our place within it</p>
       </div>
       <div className='cos-content2'>
             <ul className='cos-content2-p'>
-              <li> Join us on <span className='gradient-underline-pink'>mesmerizing stargazing expeditions</span> where we explore the celestial marvels that adorn our night sky. </li>
+              <li> Join us on <span className='gradient-underline-pinkk'>mesmerizing stargazing expeditions</span> where we explore the celestial marvels that adorn our night sky. </li>
              <li> From observing distant galaxies to tracking the movements of planets, 
              we provide opportunities to witness astronomical phenomena firsthand.</li>
              <li> Engage in thought-provoking conversations
               and debates about cosmology, astrophysics, and the future of space exploration. </li>
               <li> Our club provides a platform 
-              for intellectual discourse, where members can exchange ideas, challenge conventional wisdom, and <span className='gradient-underline-blue'>explore the 
+              for intellectual discourse, where members can exchange ideas, challenge conventional wisdom, and <span className='gradient-underline-bluee'>explore the 
               mysteries that still elude us. </span></li></ul>
             <img className='cos-content2-img' src='cos2.jpg' alt="cosmology2"/>
       </div>
       <div className='cos-content3'>
       <img className='cos-content3-img' src='cos3.jpg' alt="cosmology3"/>
             <ul className='cos-content3-p'>
-              <li>Participate in <span className='gradient-underline-light-green'>interactive workshops and demonstrations</span> where you can delve into 
+              <li>Participate in <span className='gradient-underline-light-greenn'>interactive workshops and demonstrations</span> where you can delve into 
             various aspects of astronomy and space science.</li>
               <li>Whether it's learning about astrophotography, understanding the 
             life cycles of stars, or constructing your own model rocket, we offer hands-on experiences that ignite curiosity 
